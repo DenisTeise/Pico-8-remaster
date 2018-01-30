1699d252aa2b0e853605f4ed39fabb1efcd7da79
MirroredFate <34125179+MirroredFate@users.noreply.github.com>
2018-01-30 09:22:30 +0100

    2
    2

---
+++ Assets/denis.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class denis : MonoBehaviour {
+//god dammit this is not going to wark
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
